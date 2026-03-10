01ec09d5a23d20d62f2c7ecbaa6b1f6461180b75
Manoj Chandru P M <123976902+manojchandrupm@users.noreply.github.com>
2026-03-10 12:51:25 +0530

    day 3 task update

---
+++ Day_3_task/Sentiment_analysis.py
@@ -0,0 +1,39 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import torch.nn.functional as F
+
+###### Load pretrained model name
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+
+###### Load pretrained model
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+###### Load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+###### user input loop
+while True:
+    text = input("Enter a sentence  or (type 'exit' to quit): ")
+
+    if text.lower() == "exit":
+        break
+
+    ### Converting text → tokens (convert the sentence into individual words in a list) and (assign id's for each token)
+    inputs = tokenizer(text, return_tensors="pt")   # it will return the pytorch tensor like dictionary( input_ids and attention_mask)
+
+    ### Model prediction( we will pass the dict to the model to predict the logistic values)
+    outputs = model(**inputs) # it will return the model prediction which will include the logistic values
+
+    ### Convert logits → probabilities to find out the confidence score using softmax
+    probs = F.softmax(outputs.logits, dim=1)
+
+    ### the "torch.max" will find out the max value and return its position
+    confidence, predicted_class = torch.max(probs, dim=1)
+
+    labels = ["Negative", "Positive"]
+
+    sentiment = labels[predicted_class]
+
+    print("Sentiment:", sentiment)
+    print("Confidence Score:", round(confidence.item(), 2))
+    print()
